c18702a7317f242d012d57c5cae3de3531cfe0e5
Gimeitarou <300kyen@gmail.com>
2024-09-22 09:32:13 +0900

    Revised

--- After_use.py
+++ After_use.py
@@ -4,6 +4,6 @@ f = open('D:\\pic_name.txt', 'r', encoding='UTF-8')
 basename = f.read() #read the note text of the file name of the pic
 f.close()
 
-subprocess.run('attrib -r -h d:\\Do_not_misuse.exe' #hide Do_not_misuse.exe
-                +f'&& attrib -r -h d:\\{basename}' #hide the pic
-                +'&& attrib -r -h d:\\pic_name.txt', shell=True)
+subprocess.run('attrib -r -h d:\\Do_not_misuse.exe' #unvail Do_not_misuse.exe
+                +f'&& attrib -r -h d:\\{basename}' #unvail the pic
+                +'&& attrib -r -h d:\\pic_name.txt', shell=True) #unvail the note text
